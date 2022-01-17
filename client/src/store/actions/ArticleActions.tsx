68da4910ad5acbd33270230636d1a5ff5d4fbe22
AlexGITchern <chernetsov.aa@mail.ru>
2022-01-17 20:35:43 +0300

    ..

--- client/src/store/actions/ArticleActions.tsx
+++ client/src/store/actions/ArticleActions.tsx
@@ -1,6 +1,8 @@
+import axiosApp from '../../axios/axiosApp'
 import { articleActions } from '../../types/ArticleAcrionTypes'
-import { CLEAR_ARTICLES, SET_CURRENT_CATEGORY } from './actionTypes'
-
+import { Article } from '../../types/interfaces'
+import { CLEAR_ARTICLES, SET_CURRENT_CATEGORY, SET_MY_ARTICLES } from './actionTypes'
+const token = '?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6MX0._6J5Yzv2a4JEXHOLOSvVf1kkyPsDCfbkkatcaq_uios'
 export function clearArticles(): articleActions {
   return {
     type: CLEAR_ARTICLES,
@@ -11,5 +13,30 @@ export function setCurrentCategory(category: string): articleActions {
   return {
     type: SET_CURRENT_CATEGORY,
     category,
+  }
+}
+
+export function fetchMyArticles() {
+  return async (dispatch: any) => {
+    const response = await axiosApp.get('/blog/1' + token)
+    const data = response.data
+    console.log(data);
+
+    
+  }
+}
+export function fetchArticle(id: number) {
+  return async (dispatch: any) => {
+    const response = await axiosApp.get(`/article/${id}${token}`)
+    const data = response.data
+    dispatch(setMyArticles([data]))
+    
+  }
+}
+
+export function setMyArticles(articles: Article[]): articleActions {
+  return {
+    type: SET_MY_ARTICLES,
+    articles
   }
 }
