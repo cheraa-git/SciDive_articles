a10efebb3e235c6d764c3256ffe50c20c4ca392e
Future998 <burmatov202002@gmail.com>
2022-01-18 11:40:45 +0300

    db

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { postRegisterData } from '../store/actions/AuthActions'
 
 export const AuthPage: React.FC = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     // dispatch(authDataPost({login: 'Alex', password: '111111'}))
-    // dispatch(postRegisterData({login: 'Alex', password: '111111', email: 'Alex@mail.ru'}))
+    // dispatch(postRegisterData({login: 'User1', password: '111111', email: 'User1@mail.ru'}))
   }, [dispatch])
 
   return (
